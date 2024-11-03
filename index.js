9ce2e28d0acafc5be9f5d5fe19f7d4e7ccc39aeb
Jermy <penajermy24@gmail.com>
2024-11-03 09:53:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.get('/onboarding', (req, res) => {
     res.sendFile(path.join(__dirname, 'onboarding.html'));
 });
 
+app.get('/404', (req, res) => {
+    res.sendFile(path.join(__dirname, '404.html'));
+});
+
+app.get('/tos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'tos.html'));
+});
+
+app.get('/pp', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pp.html'));
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
